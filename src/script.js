43b9ea1939e8a7f919ddb654b00ae41c35d83d01
roshan <patleroshan@gmail.com>
2023-09-24 13:45:19 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const Progress =document.getElementById('progress');
+const Song =document.getElementById('song')
+const Ctrlicon =document.getElementById('ctrlicon')
+
+Song.onloadedmetadata=function(){
+    Progress.max=Song.duration;
+Progress.value=Song.currentTime
+}
+
+
+
+function playpause(){
+
+if(Ctrlicon.classList.contains('fa-pause')){
+
+Song.pause();
+Ctrlicon.classList.remove('fa-pause');
+Ctrlicon.classList.add('fa-play');
+
+}else{
+    Song.play();
+Ctrlicon.classList.add('fa-pause');
+Ctrlicon.classList.remove('fa-play');
+
+}
+}
+
+if(Song.play()){
+setInterval(()=>{
+Progress.value=Song.currentTime;
+
+
+},500)
+
+
+}
+
+Progress.onchange=function(){
+    Song.play()
+    Song.currentTime=Progress.value;
+    Ctrlicon.classList.add('fa-pause');
+Ctrlicon.classList.remove('fa-play');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
